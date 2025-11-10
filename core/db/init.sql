37e1f15766cad78050c0cc67666e23d4ac5c37d7
LuccaHeineck <luccacheineck@gmail.com>
2025-11-10 20:46:16 -0300

    feat: add SQL init script to auto-create tables on container startup

---
+++ core/db/init.sql
@@ -0,0 +1,40 @@
+-- ./db/init.sql
+CREATE TABLE public.usuarios (
+  id_usuario SERIAL PRIMARY KEY,
+  nome TEXT NOT NULL,
+  email TEXT,
+  cpf TEXT,
+  telefone TEXT,
+  created_at TIMESTAMP DEFAULT now(),
+  senha TEXT NOT NULL
+);
+
+CREATE TABLE public.eventos (
+  id_evento SERIAL PRIMARY KEY,
+  titulo TEXT NOT NULL,
+  data_inicio DATE NOT NULL,
+  data_fim DATE NOT NULL,
+  local TEXT
+);
+
+CREATE TABLE public.inscricoes (
+  id_inscricao SERIAL PRIMARY KEY,
+  id_usuario INTEGER NOT NULL REFERENCES usuarios(id_usuario),
+  id_evento INTEGER NOT NULL REFERENCES eventos(id_evento),
+  data_inscricao TIMESTAMP DEFAULT now(),
+  data_cancelamento TIMESTAMP,
+  status BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE public.checkins (
+  id_checkin SERIAL PRIMARY KEY,
+  id_inscricao INTEGER NOT NULL REFERENCES inscricoes(id_inscricao),
+  data_checkin TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE public.certificados (
+  id_certificado SERIAL PRIMARY KEY,
+  id_checkin INTEGER NOT NULL REFERENCES checkins(id_checkin),
+  data_emissao TIMESTAMP DEFAULT now(),
+  hash_confirmacao TEXT UNIQUE
+);
